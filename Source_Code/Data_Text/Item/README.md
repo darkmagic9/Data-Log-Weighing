83c7a5207c795c74ad03a2b247d4586dcd7b43dc
[MoD]-tEAm- <darkmagic9@gmail.com>
2015-04-10 08:46:46 +0700

    Backup Source Code 20131114

--- Source_Code/Data_Text/Item/README.md
+++
@@ -1,2 +0,0 @@
-# Data-Log-Weighing
-d[>.&lt;]b
--- Source_Code/Data_Text/Report/1.TestRun.bat
+++
@@ -1,1 +0,0 @@
-Print_Report.bat "Z:\10.ALL_Project\Data Log Weighing\Source_Code\Data_Text\Report" Print_Report.jrxml T "Z:\10.ALL_Project\Data Log Weighing\Source_Code\Data_Text\Report\Data.txt"
